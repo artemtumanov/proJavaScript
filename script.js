a9159bef55f5b8037056dc0cbe7bde35d2ac93d9
Artem <artem.tumanov.93@inbox.ru>
2022-04-18 21:40:29 +0300

    add lesson -2

--- script.js
+++ script.js
@@ -5,20 +5,38 @@ const goods = [
   { title: 'Shoes', price: 250, img: 'url(img/shoes.jpg)' },
 ];
 
-const renderGoodsItem = (title = "Товар", price = "0", img = 'gray') => {
-  return `
-    <div class="goods-item" style = "background: ${img} center / cover;">
+class GoodsItem {
+  constructor({title = "Товар", price = "0", img = 'gray'}) {
+    this.title = title;
+    this.price = price;
+    this.img = img;
+  }
+  render () {
+    return `
+    <div class="goods-item" style = "background: ${this.img} center / cover;">
       <div class = "goods-item-info">
-        <div class = "goods-item-title">${title}:</div>
-        <div class = "goods-item-price">${price} &#8364</div>
+        <div class = "goods-item-title">${this.title}:</div>
+        <div class = "goods-item-price">${this.price} &#8364</div>
       </div>
       <div class="goods-item-add">Добавить</div>
     </div>`;
-};
-
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.img));
-  document.querySelector('.goods-list').innerHTML = goodsList.join(''); // .join('') - Убираем запятые (разделитель массива ",")
+  }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+  list = [];
+  fetchGoods () {
+    this.list = goods;
+  }
+  render () {
+    let goodsList = this.list.map(item => {
+      const goodsItem = new GoodsItem(item);
+      return goodsItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
