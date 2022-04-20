ec80930690dab8c9037cc3d95d45cca6cb85ae58
Artem <artem.tumanov.93@inbox.ru>
2022-04-20 20:56:50 +0300

    add lesson-3

--- script.js
+++ script.js
@@ -4,19 +4,43 @@ const goods = [
   { title: 'Jacket', price: 350, img: 'url(img/jacket.jpg)' },
   { title: 'Shoes', price: 250, img: 'url(img/shoes.jpg)' },
 ];
+const GET_GOOD_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
+const GET_GOOD_BASKET = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses//getBasket.json';
+
+const basket = document.querySelector('.basket'),
+      basketBtn = document.querySelector('.cart-button');
+let basketCount = 0;
+
+basketBtn.addEventListener('click', () => {
+  if(basketCount % 2 === 0) {
+    basket.classList.add('basket_dblock');
+    basketCount ++;
+  } else {
+    basket.classList.remove('basket_dblock');
+    basketCount ++;
+  }
+})
+
+function service(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onload = () => {
+    callback(JSON.parse(xhr.response));
+  }
+}
 
 class GoodsItem {
-  constructor({title = "Товар", price = "0", img = 'gray'}) {
-    this.title = title;
+  constructor({product_name = "Товар", price = "0"}) {
+    this.product_name = product_name;
     this.price = price;
-    this.img = img;
   }
   render () {
     return `
-    <div class="goods-item" style = "background: ${this.img} center / cover;">
+    <div class="goods-item" style="border: 1px solid black">
       <div class = "goods-item-info">
-        <div class = "goods-item-title">${this.title}:</div>
-        <div class = "goods-item-price">${this.price} &#8364</div>
+        <div class = "goods-item-title">${this.product_name}:</div>
+        <div class = "goods-item-price">${this.price} &#8381;</div>
       </div>
       <div class="goods-item-add">Добавить</div>
     </div>`;
@@ -25,8 +49,11 @@ class GoodsItem {
 
 class GoodsList {
   list = [];
-  fetchGoods () {
-    this.list = goods;
+  fetchGoods (callback) {
+    service(GET_GOOD_ITEMS, (data) => {
+      this.list = data;
+      callback();
+    });
   }
   render () {
     let goodsList = this.list.map(item => {
@@ -42,6 +69,52 @@ class GoodsList {
 }
 
 const goodsList = new GoodsList();
-goodsList.fetchGoods();
-goodsList.render();
-goodsList.sum();
+goodsList.fetchGoods(() => {
+  goodsList.render();
+  goodsList.sum();
+});
+
+class GoodsBasketItem {
+  constructor({product_name = "Товар", price = "0", quantity = "0"}) {
+    this.product_name = product_name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+  render () {
+    return `
+      <div class = "basket-item">
+        <div class = "basket-item-title">${this.product_name}</div>
+        <div class = "basket-item-price">Цена: ${this.price} &#8381;</div>
+        <div class = "basket-item-quantity"> Количество: ${this.quantity}</div>
+      </div>
+      `;
+  }
+}
+
+
+class GoodsBasket {
+  item = [];
+  fetchGoods(callback) {
+    service(GET_GOOD_BASKET, (data) => {
+      this.item = data;
+      callback();
+    })
+  }
+  render () {
+    let goodsBasketList = this.item.contents.map(item => {
+      const goodsBasketItem = new GoodsBasketItem(item);
+      return goodsBasketItem.render();
+    });
+    const basket = document.querySelector('.basket');
+    basket.innerHTML = goodsBasketList.join('');
+    basket.insertAdjacentHTML("beforeend", '<hr>');
+    basket.insertAdjacentHTML("beforeend", `<div class="basket-result">Количество товаров: ${this.item.countGoods}</div>`);
+    basket.insertAdjacentHTML("beforeend", `<div class="basket-result">Общая сумма: ${this.item.amount} &#8381</div>`);
+
+  }
+}
+
+const goodsBasket = new GoodsBasket();
+goodsBasket.fetchGoods(() => {
+  goodsBasket.render();
+});
