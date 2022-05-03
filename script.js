34c46719656434cfe1b4163198c861ebcb74efe7
Artem <artem.tumanov.93@inbox.ru>
2022-05-03 15:12:21 +0300

    add lesson-6

--- script.js
+++ script.js
@@ -6,6 +6,58 @@ function service(url) {
 }
 
 function init () {
+	const goodsItem = Vue.component('goods-item', {
+		props: ['item'],
+		template: `
+			<div class="goods-item">
+				<div class = "goods-item-info">
+					<div class = "goods-item-title">{{ item.product_name }}:</div>
+					<div class = "goods-item-price">{{ item.price }} &#8381</div>
+					</div>
+				<div class="goods-item-add">Добавить</div>
+			</div>
+		`
+	});
+
+	const goodsSearch = Vue.component('goods-search', {
+		props: ['value'],
+		template: `
+			<input id="search" 
+			v-bind:value = 'value'
+			v-on:input="$emit('input', $event.target.value)">
+		`
+	});
+
+	const customButton = Vue.component('custom-button', {
+		template: `
+			<button class="cart-button" type="button" v-on:click="$emit('click')">Корзина</button>
+		`
+	});
+
+	const basketGoods = Vue.component('basket-goods', {
+		props: ['goods', 'item'],
+		template: `
+			<div class="basket">
+				<div class = "basket-item" v-for="item in goods.contents">
+					<div class = "basket-item-title">{{ item.product_name }}</div>
+					<div class = "basket-item-price">{{ item.price }}&#8381</div>
+					<div class = "basket-item-quantity">{{ item.quantity }}</div>
+				</div>
+				<hr>
+				<div class="basket-result">Количество товаров: {{ goods.countGoods }}</div>
+				<div class="basket-result">Общая сумма: {{ goods.amount }} &#8381</div>
+			</div>
+		`
+	});
+
+	const sum = Vue.component('sum', {
+		props: ['sum'],
+		template: `
+			<div class="goods-list-sum">Итого: {{ sum }} &#8381</div>
+		`
+	});
+
+
 	const app = new Vue({
 		el: '#root',
 		data: {
